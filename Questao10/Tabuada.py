7963adf96b0466414037bfb8116fb2fc10457430
evacstz <eva.cordeiro@aluno.ce.gov.br>
2024-06-05 14:59:13 -0300

    alteração

--- Questao10/Tabuada.py
+++ Questao10/Tabuada.py
@@ -1,4 +1,4 @@
 numero = int(input("Digite um número: "))
 
-for x in range(1,11,1):
-    print(f'{numero} x {x} = {numero * x}')
+for i in range(1,11,1):
+    print(f'{numero} x {i} = {numero * i}')
